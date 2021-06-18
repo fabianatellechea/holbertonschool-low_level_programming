a9a411abf65178ef7f0a2877e1aab718d058b0ec
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-18 11:21:02 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*main - prints a string followed by a new line.
+*
+Return: 0.
+*/
+
+int main(void)
+{ puts ("\"Programming is like building a multilingual puzzle");)
+
+  return 0;
+}
+
 
