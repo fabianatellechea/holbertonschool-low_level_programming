d1a2f76901c17b3dbc7b8213c07ea25d6bf4df00
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-18 11:52:49 -0700

    spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-/** 
+/**
  *main - Prints a String followed by a new line.
- * 
+ *
  *Return: 0.
  **/
 
-int main(void) 
+int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
