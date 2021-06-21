99ee6aff6d734ea42092a1ca063abf322887e3e9
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-21 11:52:41 -0700

    numberz?

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints a string of numbers.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{ int n;
+	for (n = '0'; n <= '9' ; n++)
+		putchar (n);
+	putchar ('\n');
+
+	return (0); }
