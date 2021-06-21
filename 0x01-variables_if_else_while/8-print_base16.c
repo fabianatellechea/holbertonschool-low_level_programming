b7410bf50fbaccf5a1f56424b8228a19399ae33f
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-21 12:22:16 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers of base16.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{ int n;
+	char c;
+
+	for (n = '0'; n <= '9' ; n++)
+		putchar (n);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar (c);
+	putchar ('\n');
+
+	return (0); }
