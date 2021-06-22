6a5805e7609f29b17cb081081c83f4c3072bf3a7
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-22 14:06:28 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - prints holberton.
  *
- * Return: o.
+ * Return: 0.
  */
 
 int main(void)
-{ printf("Holberton\n");
+{ _putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 return (0);
 }
