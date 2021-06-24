5306a872ff439b128cf3fd6cb7142c45e83c08fa
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-24 09:43:59 -0700

    -

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * int _isalpha - checks for alphabet character.
+ * @c: character
+ * Return: 1 or 0.
+ */
+int _isalpha(int c)
+{
+	if (c == 'c' && c == 'C')
+	{ return (1); }
+	else
+	{ return (0); }
