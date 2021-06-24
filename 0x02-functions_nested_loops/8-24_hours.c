96d892634f1266b72c9f750211dba3f32b742adf
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-24 12:04:53 -0700

    .

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ *
+ * Return: 0.
+ */
+void jack_bauer(void)
+{ int n;
+	while (n = 00:00; n <= 23:59; n++)
+		_putchar(n);
+}
+
