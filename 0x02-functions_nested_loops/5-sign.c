8f7485ccac8af3d8722c2149c7c0a41604bae4e7
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-24 10:06:24 -0700

    .

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the sign of a number.
+ * @n: int
+ * Return: 1, 0 or -1.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{ return (1);
+		_putchar ("+");
+	}
+	else if (n == 0)
+	{ return (0);
+		_putchar ("0");
+	}
+	else
+	{ return (-1);
+		_putchar ("-");
+	}
