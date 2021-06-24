4f6aca4c1da7278cb6f42546b478e7443546b1a9
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-23 20:05:21 -0700

    .

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _islower - checks for lowercase characteres.
+ *
+ * Return: 0.
+ */
+
+int _islower(int c)
+{ if (c != 99)
+	{ return (1); }
+	else
+	{ return (0); }
+}
