e528ad3514f0208f1ff143083086281498b0132c
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-24 10:07:55 -0700

    -

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,13 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{ return (1);
-		_putchar ("+");
+		_putchar ('+');
 	}
 	else if (n == 0)
 	{ return (0);
-		_putchar ("0");
+		_putchar ('0');
 	}
 	else
 	{ return (-1);
-		_putchar ("-");
+		_putchar ('-');
 	}
