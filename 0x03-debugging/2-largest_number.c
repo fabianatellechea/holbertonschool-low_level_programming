d073f06677c09c0c91014279a82a764b4aa4b159
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-27 10:29:37 -0700

    .

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,12 +7,21 @@
 *Return: largest number
 */
 int largest_number(int a, int b, int c)
-{ int largest;
+{
+	int largest;
+
 	if (a >= b && a >= c)
-	{ largest = a; }
+	{
+		largest = a;
+	}
 	else if (b >= a && b >= c)
-	{ largest = b; }
+	{
+		largest = b;
+	}
 	else
-	{ largest = c; }
-	return (largest); } 
+	{
+		largest = c;
+	}
+	return (largest);
+}
 
