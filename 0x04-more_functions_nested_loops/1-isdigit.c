841f44eefb195139ce370637e56cb7a1cc6c6e5a
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-27 15:16:10 -0700

    .

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,14 @@
 #include <holberton.h>
+
 /**
-  *_isdigit
+  *_isdigit - checks for a digit 0-9.
   *@c: int
   *Return: 1 or 0.
   */
+
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c <= && c <= '9')
 	{ return (1); }
 	else
 	{ return (0); }
