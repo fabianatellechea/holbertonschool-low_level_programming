d66d23a1d938ec06adb891f2641054a099800ccc
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-27 15:41:37 -0700

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * print_numbers - print numbers 0-9
+  * @n: int
+  * Return: 0.
+  */
+void print_numbers(void)
+{
+	int n;
+	for (n = '0', n <= '9', n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
