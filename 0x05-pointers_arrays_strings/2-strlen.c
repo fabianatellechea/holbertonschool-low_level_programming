d892aed63a4feef3717a2e7ca529270a7ec5ba14
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-29 07:40:05 -0700

    :

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  *_strlen -  function that returns the length of a string
+  *@i: int.
+  *Return: 0.
+  */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		Return(c);
+			}
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * _puts - prints a string, followed by a new line
+  *@str: char.
+  *Return: 0.
+  */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '/0'; i++)
+	{
+		_putchar (s[i]);
+	}
+}
+
