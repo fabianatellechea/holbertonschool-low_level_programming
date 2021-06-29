df45b3aad3fa9a309b0d2958fb602d43714a4a9b
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-29 07:43:03 -0700

    .

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
   *_strlen -  function that returns the length of a string
-  *@i: int.
+  *@s: string.
   *Return: 0.
   */
 
@@ -11,7 +11,6 @@ int _strlen(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
+
 		return (0);
-			}
 }
