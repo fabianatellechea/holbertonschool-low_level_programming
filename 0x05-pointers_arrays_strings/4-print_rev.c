bde8e765a8a3fce3f63473d71d1109a87cbd0cd2
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-29 10:36:15 -0700

    -

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,11 @@ void print_rev(char *s)
 
 		for (i = 0; s[i] != '\0'; i++)
 			;
-		for (i >= 0; i--)
-		{
-			_putchar (s[i]);
-		}
+		for (i = s; i >= 0; i--)
+{
+		_putchar (s[i]);
 
+		i--;
+}
 		_putchar ('\n');
 }
