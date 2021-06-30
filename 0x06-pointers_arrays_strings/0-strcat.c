55741905336330dc5bd1402f8373db72dab69752
fabianatellechea <fabianagtellechea@gmail.com>
2021-06-30 13:22:47 -0700

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,16 +7,23 @@
   */
 char *_strcat(char *dest, char *src)
 {
-	char d = *dest;
-	char s = *src;
+	int d, s;
 
-	while (*(++d))
-		;
-	while (*(d++) = *(s++))
-		;
-	_putchar (d);
-	return (0);
+	d = 0;
+	while(dest[d] != '\0')
+	{
+		d++;
+	}
+
+	s = 0;
+
+	while(src[s] != '\0')
+
+	{	dest[d] = src[s];
+		        d++;
+			        s++;
+				dest[d] = '\0';
+	}	
+				return (dest);
 }
-
-
 
