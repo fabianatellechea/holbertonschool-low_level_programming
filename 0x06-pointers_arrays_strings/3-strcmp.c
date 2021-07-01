3b12ade3fabefc86d4a13a2c75f19a59b57b5085
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-01 10:19:54 -0700

    .

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * _strcmp - compares two strings.
+  * @s1: char1
+  * @s2: char2
+  *Return: 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	if (s1[i] && s2[i] != '\0')
+	{
+		return (s1[i] - s2[i]);
+	}
+
+	i++;
+	return (0);
+
+}
+
