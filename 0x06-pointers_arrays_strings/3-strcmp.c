4eba0cd3bb9f0e1cb93917d4ea6919bfdb44eba3
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-01 13:14:40 -0700

    .

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,13 @@ int _strcmp(char *s1, char *s2)
 
 	if (s1[i] && s2[i] != '\0')
 	{
-		return (s1[i] - s2[i]);
-		i++;
-}
 
-return (0);
+	return (s1[i] - s2[i]);
+	++i;
+
+	}
+
+	return (0);
 
 }
 
