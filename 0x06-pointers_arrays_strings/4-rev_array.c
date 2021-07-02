11013c5902548de05e57dfbc6d6c3fd10cbe3dcf
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-01 18:38:40 -0700

    .

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+  *reverse_array - reverses the content of an array of integers
+  *@a: int1
+  *@n: int2
+  *Return: 0.
+  */
+
+void reverse_array(int *a, int n)
+{
+	int b;
+
+	b = 0;
+
+	if (a[b] != '\0')
+	{
+		b++;
+	}
+	if (a[n] != '\0')
+
+		b++;
+		n--;
+	}
+
