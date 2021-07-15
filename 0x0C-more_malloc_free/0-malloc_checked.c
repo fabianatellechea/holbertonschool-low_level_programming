00d6baa4d368609eeaa364a149480c4210bdc491
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-15 09:54:41 -0700

    .

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+  * malloc_checked - function that allocates memory
+  *@b: unsigned int
+  *Return: a pointer to the allocated memory
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	
