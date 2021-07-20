ff7a0786b4ab954624890d9a16fba892757bd104
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-20 05:59:20 -0700

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef STRUCTFILE
-#define STRUCTFILE
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - struct dog
@@ -9,9 +9,9 @@
  */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
