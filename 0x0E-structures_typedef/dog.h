ccac026c403db52897659bfd7aa1ffaf96fbe922
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-20 05:52:47 -0700

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
   * struct dog - struct dog
   *@name: char
   *@age: floar
   *@owner: char
   */
-
 struct dog
 {
 	char * = name;
 	float = age;
 	char * = owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
