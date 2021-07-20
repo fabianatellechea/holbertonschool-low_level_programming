8458b49a5a1945488e01ef120cdb828382e7e726
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-20 06:26:56 -0700

    .

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+  * @d: dog
+  * @name: char
+  * @age: float
+  * @owner: char
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if(d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
