5064ec656bd9fad37dc561371afe9caeae0ce099
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-20 07:19:46 -0700

    .

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+   *free_dog - function that frees dogs
+   *@d: d
+   */
+
+void free_dog(dog_t *d)
+{
+	dog_t *my_dog = d;
+
+	if (d)
+		free(my_dog->name);
+	free(my_dog->owner);
+	free(my_dog);
+}
+
+
+
+
