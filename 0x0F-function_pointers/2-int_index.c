3854bc8d74fa0f4b8fc935c8be1c240071251fe8
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-21 13:30:00 -0700

    .

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+  *int_index -  function that searches for an integer
+  *@array: array
+  *@size: int
+  */
+
+int int_index(int *array, int size, int (*cmp)(int));
+{
+
