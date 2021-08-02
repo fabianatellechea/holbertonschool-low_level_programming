5d6658925acc817c41c816752fc1ea1f975c13ee
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-02 13:10:12 -0700

    .

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+  *print_list - function that prints all the elements of a list_t list.
+  *@h: h
+  *Return: the number of nodes.
+  */
+
+size_t print_list(const list_t *h)
+{
+
+
+	for (i = 0, h != '\0', i++)
+
+	if (str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+
+
