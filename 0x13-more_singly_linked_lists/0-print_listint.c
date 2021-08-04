728245381723f050bc9269189864308c3f45ad80
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-04 06:42:42 -0700

    .

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
   *print_listint - function that prints all the elements of a listint_t list.
@@ -8,4 +9,19 @@
 
 size_t print_listint(const listint_t *h)
 {
+	size_t i = 0;
+
+	if (!h)
+	{
+		return (i);
+	}
+
+		else
+		{
+			printf("%d\n", h->n);
+		}
+
+			return (print_listint(h->next) + 1);
+	}
+
 
