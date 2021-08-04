f18988dd32f92b0ce54bf92631e1da5e6bdaab2f
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-04 06:44:20 -0700

    betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,8 +3,8 @@
 
 /**
   *print_listint - function that prints all the elements of a listint_t list.
-  *
-  *
+  *@h: h
+  *Return: the number of nodes
   */
 
 size_t print_listint(const listint_t *h)
