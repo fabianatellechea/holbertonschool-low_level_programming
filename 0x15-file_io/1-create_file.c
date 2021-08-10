57ca5db754e20873c1d3d71cf88fca0df41465a4
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-10 10:17:38 -0700

    .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,19 +19,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (l = 0; text_content[l]; l++)
+		for (l = 0; text_content[l] != '\0'; l++)
 			;
 	}
 	else
 	{
-		o = open(filename, O_CREAT, 600);
+		o = open(filename, O_CREAT, 0600);
 		return (1);
 	}
 
 	if (filename)
 	{
 		m = malloc(sizeof(char) * l);
-		o = open(filename, O_CREAT, O_TRUNC, O_WRONLY, 600);
+		o = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 		x = write(o, text_content, l);
 		return (1);
 	}
