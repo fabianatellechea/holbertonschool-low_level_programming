a81d88485abd2fc7d8e865eea004bc6823719e33
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-10 06:38:48 -0700

    .

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,9 @@
+#include <stddef.h>
+#include <stdlib.h>
+
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
