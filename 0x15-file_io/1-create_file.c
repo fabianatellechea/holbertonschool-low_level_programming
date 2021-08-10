c3333273e34d8609bd6185251c8d9b206e0f8e9f
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-10 10:03:25 -0700

    .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  *create_file - 
+  *create_file - function that creates a file
   *@filename:the name of the file to create
   *@text_content:is a NULL terminated string to write to the file
   *Return: 1 on success, -1 on failure
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (filename)
-	{	
+	{
 		m = malloc(sizeof(char) * l);
 		o = open(filename, O_CREAT, O_TRUNC, O_WRONLY, 600);
 		x = write(o, text_content, l);
@@ -46,5 +46,5 @@ int create_file(const char *filename, char *text_content)
 }
 	free(m);
 	close(o);
-	return(1);
+	return (1);
 }
