8b0ddbcbf34e9cd7e8a231a80c81500a830677a2
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-19 09:23:07 -0700

    .

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,19 +10,17 @@ int print_last_digit(int v)
 {
 	int ld = v % 10;
 
-	if (v < 0)
+	if (v > 0)
 	{
 		_putchar(ld + '0');
 		return (ld);
 	}
-	
+
 	else if (v == 0)
 	{
 		_putchar(v + '0');
 		return (v);
 	}
-
 	_putchar((ld * (-1)) + '0');
 	return (ld * (-1));
-
 }
