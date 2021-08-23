2f5c320f6325b7d5ba4b3b0e09079a7a8476303b
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-23 12:56:51 -0700

    :

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *_strlen_recursion - function that returns the length of a string
+  *@s: string
+  *Return: 0
+  */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	return (0);
+
+	i = 1 + _strlen_recursion(s + 1);
+
+	return (i);
+}
