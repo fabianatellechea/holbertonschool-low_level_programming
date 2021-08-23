555bb538195594aecf47746f72c3b7f418f207ab
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-23 06:17:43 -0700

    :

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  *print_chessboard - function that prints the chessboard
+  *@a: a
+  *Return: 0
+  */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+	_putchar('\n');
+	}
+}
