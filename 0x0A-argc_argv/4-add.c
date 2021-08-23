cd96f2d12d5afc5c89fdd4d6b8d2797b4202f7fc
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-23 16:37:17 -0700

    .

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - main
+  *@argc: argc
+  *@argv: argv
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int main(int argc, char *argv[])
+{
+	int num, dig, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (dig = 0; argv[num][dig]; dig++)
+		{
+			if (argv[num][dig] < '0' || argv[num][dig] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
