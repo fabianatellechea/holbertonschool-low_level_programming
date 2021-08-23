b6c2cffd045f17fc734f1c35e3954200014a890e
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-23 13:41:29 -0700

    :

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	if (s1[i] && s2[i] != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-
-	return (s1[i] - s2[i]);
-	++i;
-
+		if (*s1 > *s2)
+			return (*s1 - *s2);
+		else if (*s2 > *s1)
+			return ((*s2 - *s1) * -1);
+		s1++;
+		s2++;
 	}
-
 	return (0);
 
 }
