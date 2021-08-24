35b39acf0b881c97d61eaee8049ae3e97bc55465
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-23 18:03:29 -0700

    .

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - function that frees a 2 dimensional grid
+  * @grid: grid
+  * @height: height of grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
