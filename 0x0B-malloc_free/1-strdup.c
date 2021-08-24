d786c35a7f741f211e975cf7ba5fd9f67acee862
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-23 17:13:44 -0700

    .

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 
 	if (str)
 	{
-		for (length = 0; str[i] != '\0'; length++)
+		for (length = 0; str[length] != '\0'; length++)
 		;
 		p = malloc(sizeof(char) * length + 1);
 
