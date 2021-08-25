026dd4691df1ec6942f8d1a264087de67e2bbd45
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-25 07:39:31 -0700

    :

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * malloc_checked - function that allocates memory
-  *@b: unsigned int
-  *Return: a pointer to the allocated memory
+  *malloc_checked - function that allocates memory using malloc
+  *@b: u int
+  *Return: pointer to the allocated memory
   */
 
 void *malloc_checked(unsigned int b)
 {
-	
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
+
