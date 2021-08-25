be613866d301c537396988ec30b4c0c6209b4202
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-25 06:56:47 -0700

    :

---
+++ 0x0B-malloc_free/0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *malloc_checked - function that allocates memory using malloc
+  *@b: unsigned int
+  *Return: pointer to the allocated memory
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (!p)
+		exit(98);
+	return (p);
+}
