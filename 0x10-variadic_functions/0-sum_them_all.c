61dfd57b5744c8d1b0faa46a26e6112b274a3970
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-25 08:22:15 -0700

    :

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,10 +2,30 @@
 
 /**
   *sum_them_all -  function that returns the sum of all its parameters
-  *
-  *
+  *@n: n
+  *Return: sum
   */
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list op;
+	int sum = 0, i = 0;
+
+	va_start(op, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	while (i < (int)n)
+	{
+		sum += va_arg(op, int);
+		i++;
+	}
+
+	va_end(op);
+
+	return (sum);
+}
 
