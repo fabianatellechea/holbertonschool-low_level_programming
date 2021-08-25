e574736408e5fa90bd4cd2c920a26eb0b7ed04ab
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-25 08:29:03 -0700

    :

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ *print_all - function
+ *@format: format
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int n = 0;
+	int i = 0;
+	char *comma = ", ";
+	char *c;
+
+	va_start(list, format);
+
+	while (format && format[i])
+		i++;
+
+	while (format && format[n])
+	{
+		if (n == (i - 1))
+		{
+			comma = "";
+		}
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(list, int), comma);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(list, int), comma);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(list, double), comma);
+			break;
+		case 's':
+			c = va_arg(list, char *);
+			if (c == NULL)
+				c = "(nil)";
+					printf("%s%s", c, comma);
+			break;
+		}
+		n++;
+	}
+	printf("\n");
+	va_end(list);
+}
