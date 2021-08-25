2ad52bb91eaba9a2fdbc2d164f8d5b6dcaee7aed
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-25 07:49:47 -0700

    :

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - uyy
+* @nmemb: duele.
+* @size: la.
+* Return: cola
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i = 0, j;
+
+	j = nmemb * size;
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+        p = malloc(j);
+	if (p == NULL)
+		return (NULL);
+
+	while (i <j)
+	{
+		p[i] = 0;
+                i++;
+	}
+	return (p);
+}
