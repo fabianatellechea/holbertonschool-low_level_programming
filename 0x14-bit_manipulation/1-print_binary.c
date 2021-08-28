9c5b8fd62487ca985a09f63ad9d551fffa6ceb37
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-28 11:08:32 -0700

    .

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *print_binary - print binary
+  *@n: un long int
+  */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0 || n == 1)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		print_binary(n >> 1);
+		_putchar('0' + (n & 1));
+	}
+}
