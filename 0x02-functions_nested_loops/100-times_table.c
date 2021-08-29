b72f8511d8b82d755514a9c0cdb319d6e99a5f86
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-29 15:34:29 -0700

    ·

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *print_times_table - function
+ *@n: int
+ *
+ *Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if ((n >= 0) && (n <= 15))
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (b > 0)
+				{
+					_putchar(' ');
+					if (c > 99)
+						_putchar((c / 100) + '0');
+					else
+						_putchar(' ');
+					if (c > 9)
+						_putchar(((c / 10) % 10) + '0');
+					else
+						_putchar(' ');
+				}
+				_putchar((c % 10) + '0');
+				if (b < n)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
