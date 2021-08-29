dccdf67d6f8a9e3f158be52b84a0a2cd90199bcd
fabianatellechea <fabianagtellechea@gmail.com>
2021-08-29 15:18:59 -0700

    :

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,18 +5,20 @@
  *
  *Return: 0
  */
+
 int main(void)
 {
-	int i, i_;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (i_ = i; i_ <= 9; i_++)
+		for (j = i; j <= 9; j++)
 		{
-			if (i != i_)
+			if (i != j)
 			{
 				putchar(i + '0');
-				putchar(i_ + '0');
+				putchar(j + '0');
+
 				if (i < 8)
 				{
 					putchar(',');
@@ -25,6 +27,7 @@ int main(void)
 			}
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
