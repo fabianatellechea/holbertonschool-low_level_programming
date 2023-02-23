67b8d0e9952d770748da0e0e4c0da13db43f4d70
fabianatellechea <fabianatellechea@gmail.com>
2023-02-23 07:13:09 -0800

    agus

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i % 3))
+			printf("Fizz");
+		if (!(i % 5))
+			printf("Buzz");
+		else if ((i % 3) && (i % 5))
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+			}
+			printf("\n");
+			}
