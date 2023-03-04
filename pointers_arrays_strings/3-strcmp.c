8885aa612d98486870eb216fe2fc2a613a7c7096
fabianatellechea <fabianatellechea@gmail.com>
2023-03-04 08:13:57 -0800

    .

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: string1
+ * @s2: string2
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 > *s2)
+		{
+			return (*s1 - *s2);
+		}
+
+		else
+		{
+			return ((*s2 - *s1) * -1);
+		}
+
+		s1++;
+		s2++;
+	}
+	return (0);
+}
