a732b699b062f2837ca6286c60fda0d8b40aacdc
fabianatellechea <fabianatellechea@gmail.com>
2023-03-06 09:32:54 -0800

    pow

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - the value of x raised to the power of y
+ * @x: value
+ * @y: power value
+ * Return: x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (_pow_recursion(x, y - 1) * x);
+	}
+}
