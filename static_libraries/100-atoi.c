01a76589d919bc87255a923f1710e8bfb71e98c1
fabianatellechea <fabianatellechea@gmail.com>
2023-03-07 05:58:09 -0800

    lib

---
+++ static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to char
+ * @b: char
+ * @n: unsigned int
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+}
---
+++ static_libraries/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to char
+ * @b: char
+ * @n: unsigned int
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+}
