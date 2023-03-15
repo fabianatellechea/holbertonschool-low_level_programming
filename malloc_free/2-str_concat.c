0d6b28e81d75f6a0d9c60ca8189425c0c6838a00
fabianatellechea <fabianatellechea@gmail.com>
2023-03-15 16:00:55 -0700

    betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,13 +14,13 @@ char *str_concat(char *s1, char *s2)
 	char *p = NULL;
 
 	if (s1 == NULL)
-        {
-                s1 = "";
-        }
-        if (s2 == NULL)
-        {
-                s2 = "";
-        }
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
 	if (s1 && s2)
 	{
