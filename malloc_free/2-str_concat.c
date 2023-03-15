ec64ff0af78c1c369dd1bd9688d8c9384881e657
fabianatellechea <fabianatellechea@gmail.com>
2023-03-15 15:58:50 -0700

    concat?

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,10 +13,15 @@ char *str_concat(char *s1, char *s2)
 	int h, i, j, k, length;
 	char *p = NULL;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+        {
+                s1 = "";
+        }
+        if (s2 == NULL)
+        {
+                s2 = "";
+        }
+
 	if (s1 && s2)
 	{
 		for (i = 0; s1[i]; i++)
@@ -27,6 +32,7 @@ char *str_concat(char *s1, char *s2)
 		length = i + j;
 		p = malloc(sizeof(char) * length + 1);
 	}
+
 	if (p)
 	{
 		k = 0;
@@ -39,5 +45,6 @@ char *str_concat(char *s1, char *s2)
 			p[h] = s2[k++];
 		}
 	}
+
 	return (p);
 }
