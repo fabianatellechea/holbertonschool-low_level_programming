1b61cace34c49afd153b6aa805b48c4ab441a277
fabianatellechea <fabianatellechea@gmail.com>
2023-03-26 12:39:05 -0700

    iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+  *array_iterator - function that executes a function given as a parameter
+  *@array: array
+  *@size: size
+  *@action: function pointer
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
