6ece7f798ad22a8868a3e8109f832f6aaaf6ee53
fabianatellechea <fabianatellechea@gmail.com>
2023-04-15 16:27:56 -0700

    append 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename)
 	{
-		o = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		o = open(filename, O_APPEND | O_WRONLY);
 		w = write(o, text_content, i);
 
 
